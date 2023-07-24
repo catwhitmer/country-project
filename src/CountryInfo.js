98d06192fe3a8478981e810b331f239325488f69
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2023-07-24 17:14:37 -0400

    updated api call, added search query, and added class names for styling

--- src/CountryInfo.js
+++ src/CountryInfo.js
@@ -1,80 +1,66 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import styled from "styled-components";
-
-const SearchBar = styled.input`
-  padding: 8px;
-  margin-bottom: 10px;
-`;
-
-const Panel = styled.div`
-  border: 1px solid #ccc;
-  padding: 10px;
-  margin-bottom: 10px;
-`;
-
-const Image = styled.img`
-  max-width: 100%;
-  max-height: 200px;
-`;
-
 
 const CountryInfo = () => {
+  const [searchQuery, setSearchQuery] = useState("");
   const [countryData, setCountryData] = useState(null);
   const [error, setError] = useState(null);
-  const [searchQuery, setSearchQuery] = useState("");
-
-  useEffect(() => {
-    const fetchCountryData = async () => {
-      try {
-        const response = await axios.get("https://restcountries.com/v3/all");
-        console.log(response.data);
-        setCountryData(response.data);
-      } catch (error) {
-        setError("Error fetching data from the API");
-      }
-    };
-    fetchCountryData();
-  }, []);
 
   const handleSearchChange = (event) => {
     setSearchQuery(event.target.value);
   };
 
-  if (error) {
-    return <div>{error}</div>;
-  }
-
-  if (!countryData) {
-    return <div>Loading...</div>;
-  }
-
-  const filteredCountries = countryData.filter((country) => {
-    return (
-      country.name.common.toLowerCase().includes(searchQuery.toLowerCase()) ||
-      country.name.official.toLowerCase().includes(searchQuery.toLowerCase())
-    );
-  });
+  const handleSearchSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.get(
+        `https://restcountries.com/v3/name/${searchQuery}`
+      );
+      console.log(response.data);
+      setCountryData(response.data);
+    } catch (error) {
+      setError("Error fetching data from the API");
+    }
+  };
 
   return (
     <div>
-      <SearchBar
-        type="text"
-        placeholder="Search for a country..."
-        value={searchQuery}
-        onChange={handleSearchChange}
-      />
-      {filteredCountries.map((country) => (
-        <div key={country.cca3}>
-          <Panel>
-            <h2>{country.name.common}</h2>
-            <h2>{country.name.official}</h2>
-          </Panel>
-          <Panel>
-            <Image src={country.flags[0]} alt={`${country.name.common} Flag`} />
-          </Panel>
-        </div>
-      ))}
+      <form className="form" onSubmit={handleSearchSubmit}>
+        <input
+          type="text"
+          placeholder="Search for a country..."
+          value={searchQuery}
+          onChange={handleSearchChange}
+        />
+        <button type="submit">Search</button>
+      </form>
+      {countryData &&
+        countryData.map((country) => (
+          <div key={country.cca3}>
+            <div className="panelContainer">
+              <div className="firstPanel">
+                <h2>{country.name.common}</h2>
+                <h2>{country.name.official}</h2>
+                {/* <h2>{currency}</h2>    */}
+                {/* <h2>{currency symbol}</h2> */}
+                {/* <h2>{language}</h2> */}
+                <h2>{country.capital}</h2>
+                <h2>{country.population}</h2>
+              </div>
+              <div className="secondPanel">
+                <img className="flag"
+                  src={country.flags[0]}
+                  alt={`${country.name.common} Flag`}
+                />
+                <img className=""
+                  src={country.coatofarms}
+                  alt={`${country.name.common} Coat of Arms`}
+                />
+              </div>
+            </div>
+          </div>
+        ))}
+      {error && <div>{error}</div>}
     </div>
   );
 };
